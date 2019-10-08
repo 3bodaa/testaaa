7b17efdb7e2d2ce73319b6234d0aef20cb372563
3bodaa <56321214+3bodaa@users.noreply.github.com>
2019-10-08 23:28:00 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 console.log("TG Master");
-var prefix = "!";
+var prefix = "#";
 client.on("message", message => {
 
             if (message.content.startsWith(prefix + "bc")) {
